26204e5197a55f728831561948a69b11e6cca350
Ángel Quiroz <aquirozdev@gmail.com>
2023-09-19 21:47:28 -0500

    feat: setup shadcn-ui

---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange>
+      {children}
+    </ThemeProvider>
+  );
+}
